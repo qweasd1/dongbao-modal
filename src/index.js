da9fa8c40bb3ee2c162b98ad1390fb64c365e3eb
tony <zhw65@pitt.edu>
2017-04-07 23:31:10 -0400

    fix State not import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * Created by tony on 4/7/17.
  */
 import React from 'react'
-import {connect} from 'dongbao'
+import {State, connect} from 'dongbao'
 
 // const
 let EASE_FUNCTION_ELASTIC = "cubic-bezier(.03,1.6,.46,1.6)"
